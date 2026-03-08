fd2011f5fae4e3333228c76c2bc27520d04f6381
BinuraBimsara <binurabimsara04@gmail.com>
2026-03-09 00:52:00 +0530

    feat(repo): import dart:math for distance calculations

--- frontend/lib/features/complaints/data/repositories/firestore_complaint_repository.dart
+++ frontend/lib/features/complaints/data/repositories/firestore_complaint_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:image_picker/image_picker.dart';
